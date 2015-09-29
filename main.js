3833820519ff69812d34f93e5d8b5d95af165329
Tijmen Bok <mikauschekzen@gmail.com>
2015-09-29 02:40:52 +0200

    added cleaning duty

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var path = require("path");
 var spawn = require("child_process").spawnSync;
 var process = require("process");
 var glob = require("glob").sync;
+var fs = require("fs");
 
 var task = process.argv[2];
 
@@ -10,7 +11,7 @@ spawn("youtube-dl", ["-a", "list.txt", "-o", "input/%(title)s.%(ext)s", "--restr
 console.log("Video download complete");
 
 var files = glob("input/*.+(mp4|flv)");
-var a, file, savePath;
+var a, file, savePath, cleaningList = [];
 if(task !== undefined) {
 	switch(task) {
 		case "rip-mp3":
@@ -18,6 +19,7 @@ if(task !== undefined) {
 		case "ripmp3":
 			for(a = 0;a < files.length;a++) {
 				file = files[a];
+				cleaningList.push(file);
 				savePath = "output_music/" + path.basename(file, path.extname(file)) + ".mp3";
 				console.log("Processing file " + savePath);
 				spawn("ffmpeg", ["-i", file, "-acodec", "libmp3lame", "-ab", "128", savePath]);
@@ -25,3 +27,11 @@ if(task !== undefined) {
 			break;
 	}
 }
+
+// Clean up afterwards
+console.log("Cleaning...");
+for(a = 0;a < cleaningList.length;a++) {
+	file = cleaningList[a];
+	fs.unlinkSync(file);
+}
+console.log("Done!");
